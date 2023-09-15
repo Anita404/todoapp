ff491a11ebfe4597ec406ccb8bba845e38e96542
Anita Lekova <anita.lekova@xeneta.com>
2023-09-15 12:16:39 +0200

    Add Structure to List

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import React from "react";
+import { Title } from "./App.styles";
+import TaskCreation from "./TaskCreation";
+import ToDoList from "./ToDoList";
 
 export default function App() {
-  return <h1> Hello Anita! </h1>;
+  return (
+    <StyledPageContainer>
+      <HeadingContainer>
+        To Do App
+        <icon />
+        {/* would be at the edge of the heading container  */}
+      </HeadingContainer>
+      <TaskCreation></TaskCreation>
+      <ToDoList>
+        <StatsContainer></StatsContainer>
+        {/* will be in the styles for toDoList */}
+      </ToDoList>
+      <Instructions> </Instructions>
+    </StyledPageContainer>
+  );
 }
