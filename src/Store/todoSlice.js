f9ceb1d5ef16738bf622fbdeecb8cf36b583ff40
Anita Lekova <anita.lekova@xeneta.com>
2023-10-15 12:34:59 +0200

    Figuring out state change for removal

--- src/Store/todoSlice.js
+++ src/Store/todoSlice.js
@@ -1,6 +1,6 @@
 const initialState = {
   todos: [
-    { id: 1, activity: "Jog around and find out", status: false },
+    { id: 1, activity: "Jog around and find out", status: true },
     { id: 2, activity: "Feed spiteful cat", status: true },
     { id: 3, activity: "Learn frontend w/Frontend God", status: true },
     { id: 4, activity: "Figure out life", status: true },
@@ -14,10 +14,13 @@ const todoSlice = (state = initialState, action) => {
         ...state,
         todos: [...state.todos, { activity: action.payload, status: true }],
       };
-    case "removeTodo": //need to change status to false targetting specific id upon click // dispatch - have to add smtng 
+    case "removeTodo": //need to change status to false targetting specific id upon click // dispatch - have to add smtng
       return {
         ...state,
-        todos: [...state.todos, { id:  }],
+        todos: [
+          ...state.todos.filter((todos) => todos !== action.payload),
+          { id: action.payload, status: false },
+        ],
       };
     default:
       return initialState;
